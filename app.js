7ee243b568e4253b51b05e0035595a93c4c64593
Alberto Heissl <alberto@MacBook-Air-di-Alberto.local>
2021-08-12 17:27:13 +0200

    init

---
+++ app.js
@@ -0,0 +1,8 @@
+const imageUrl = new URLSearchParams(window.location.search).get('img');
+if (imageUrl) {
+    const panorama = new PANOLENS.ImagePanorama( imageUrl );
+    const viewer = new PANOLENS.Viewer({ container: document.querySelector( '#panolens-separate-container' )});
+    viewer.add( panorama );
+} else {
+    console.warn('No image found')
+}
